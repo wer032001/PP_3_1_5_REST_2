36437b8b6f8381a7e1dffe62e242c5bf22b4300c
wer032001 <wer032001@gmail.com>
2023-02-02 15:37:39 +0300

    task 3.1.3 completed

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -1,16 +1,7 @@
 package ru.kata.spring.boot_security.demo;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.kata.spring.boot_security.demo.entity.Role;
-import ru.kata.spring.boot_security.demo.entity.User;
-import ru.kata.spring.boot_security.demo.repository.UserRepository;
-import ru.kata.spring.boot_security.demo.service.UserService;
-import ru.kata.spring.boot_security.demo.service.UserServiceImp;
-
-import java.util.Set;
 
 @SpringBootApplication
 public class SpringBootSecurityDemoApplication {
