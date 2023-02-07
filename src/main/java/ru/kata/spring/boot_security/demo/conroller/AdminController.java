48d340544eddfcf36a7420926f56691a9a5e3276
wer032001 <wer032001@gmail.com>
2023-02-07 12:53:49 +0300

    Task 3.1.4 completed final

--- src/main/java/ru/kata/spring/boot_security/demo/conroller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/conroller/AdminController.java
@@ -37,18 +37,6 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-//    @GetMapping("/admin/edit/{id}")
-//    public String updateUserForm(@PathVariable Long id, Model model) {
-//        model.addAttribute("user", userService.getUserById(id));
-//        return "updateuser";
-//    }
-
-
-//    @PostMapping("/admin/{id}")
-//    public String updateUser(@PathVariable Long id, @ModelAttribute("user") User user) {
-//        userService.updateUser(user, id);
-//        return "redirect:/admin";
-//    }
     @GetMapping("/admin/getOne")
     @ResponseBody
     public User getOne(Long id) {
